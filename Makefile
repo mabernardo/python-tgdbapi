8cf1a59886e2d3131d5cf3062d2eb3175a4c1127
Marcio Bernardo <mabernardo@gmail.com>
2016-12-28 19:57:51 -0200

    Makefile and setup

--- Makefile
+++ Makefile
@@ -1,5 +1,24 @@
-init:
-	pip3 install -r requirements.txt
+.PHONY: create clean install test
+
+create:
+	python3 -m venv .venv
+
+clean:
+	rm -rf .venv
+
+ifneq ($(strip $(VIRTUAL_ENV)),)
+
+install:
+	pip install --upgrade pip
+	pip install -r requirements.txt
 
 test:
-	nosetests --with-coverage --cover-tests --cover-erase --cover-package=tgdbapi 
+	nosetests --with-coverage --cover-tests --cover-html --cover-erase --cover-package=tgdbapi
+
+else
+
+install: ;@echo 'Virtual Env not activated'
+
+test: install
+
+endif
---
+++ setup.py
@@ -0,0 +1,64 @@
+from __future__ import print_function
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+import io
+import codecs
+import os
+import sys
+
+import tgbdapi
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+def read(*filenames, **kwargs):
+    encoding = kwargs.get('encoding', 'utf-8')
+    sep = kwargs.get('sep', '\n')
+    buf = []
+    for filename in filenames:
+        with io.open(filename, encoding=encoding) as f:
+            buf.append(f.read())
+    return sep.join(buf)
+
+long_description = read('README.rst')
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        errcode = pytest.main(self.test_args)
+        sys.exit(errcode)
+
+setup(
+    name='python-tgdbapi',
+    version=tgdbapi.__version__,
+    url='http://github.com/mabernardo/python-tgdbapi/',
+    license='MIT License',
+    author='Marcio Bernardo',
+    tests_require=['pytest'],
+    install_requires=[],
+    cmdclass={'test': PyTest},
+    author_email='mabernardo@gmail.com',
+    description='A comprehensive client for the Games DB API',
+    long_description=long_description,
+    packages=['tgdbapi'],
+    include_package_data=True,
+    platforms='any',
+    test_suite='tgdbapi.test.test_tgdbapi',
+    classifiers = [
+        'Programming Language :: Python',
+        'Development Status :: 4 - Beta',
+        'Natural Language :: English',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        ],
+    extras_require={
+        'testing': ['pytest'],
+    }
+)
